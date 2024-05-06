347472af868eefe2bd99057d16e36a215e2abd4d
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-05 23:25:50 -0500

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TaskComponent } from './components/task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'task', component: TaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
