d043fdbfdeb38b56ca3474851758a81d4853cd5b
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-06 18:36:18 -0500

    Installed Cors

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  constructor(
+    protected http: HttpClient
+    ) { }
 
+    addUser(usr:any):Observable<any>{
+      let route = [environment.apiUrl, 'user'].join('/');
+      return this.http.post(route,usr);
+    }
 }
