21cb2b23a99c72664a596859c453e3d295fa6e03
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-06 19:42:08 -0500

    Create task.service.ts

---
+++ frontend/src/app/services/task/task.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(
+    protected http: HttpClient
+  ) { }
+
+  getAllTasks(): Observable<any> {
+    let route = [environment.apiUrl, 'task'].join('/');
+    return this.http.get(route);
+  };
+  getTaskById(id: any): Observable<any> {
+    let route = [environment.apiUrl, 'task', id].join('/');
+    return this.http.get(route);
+  };
+  addTask(task: any): Observable<any> {
+    let route = [environment.apiUrl, 'task'].join('/');
+    return this.http.post(route, task);
+  };
+  editTask(task: any, id: any): Observable<any> {
+    let route = [environment.apiUrl, 'task', id].join('/');
+    return this.http.put(route, task);
+  };
+  deleteTask(id: any): Observable<any> {
+    let route = [environment.apiUrl, 'task', id].join('/');
+    return this.http.delete(route);
+  }
+}
