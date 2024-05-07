9a32dce7d01029b24a1a6dbfecce7d43b3bf915c
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-06 22:47:38 -0500

    Update register.component.ts

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, ActivatedRoute, ParamMap } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  constructor(private UserService: UserService) { }
 
+  registerUser(event: Event): void {
+    event.preventDefault();
+    const fullName = (event.target as HTMLFormElement).elements.namedItem('fullName');
+    const email = (event.target as HTMLFormElement).elements.namedItem('email');
+    const password = (event.target as HTMLFormElement).elements.namedItem('password');
+
+    const newUser = { fullName, email, password };
+    this.UserService.addUser(newUser).subscribe(
+      (response) => {
+        console.log('User registered successfully:', response);
+        // Aquí puedes agregar lógica adicional después de registrar al usuario
+      },
+      (error) => {
+        console.error('Error registering user:', error);
+        // Aquí puedes manejar errores si ocurren durante el registro
+      }
+    );
+  }
 }
