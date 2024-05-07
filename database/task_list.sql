079a4c7c6df6d6b9e73ae81c73e9c96de066754c
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-07 15:23:22 -0500

    Update task_list.sql

--- database/task_list.sql
+++ database/task_list.sql
@@ -1,22 +1,11 @@
 -- Table: public.task_list
-
 -- DROP TABLE IF EXISTS public.task_list;
-
-CREATE TABLE IF NOT EXISTS public.task_list
-(
-    id bigint NOT NULL DEFAULT nextval('task_list_id_seq'::regclass),
+CREATE TABLE IF NOT EXISTS public.task_list (
+    id bigserial NOT NULL,
     title text COLLATE pg_catalog."default" NOT NULL,
     user_id bigint NOT NULL,
-    create_at date NOT NULL,
+    create_at date DEFAULT CURRENT_DATE,
     CONSTRAINT task_list_pkey PRIMARY KEY (id),
-    CONSTRAINT task_list_user_id_fkey FOREIGN KEY (user_id)
-        REFERENCES public."user" (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.task_list
-    OWNER to admin;
+    CONSTRAINT task_list_user_id_fkey FOREIGN KEY (user_id) REFERENCES public."user" (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.task_list OWNER to admin;
