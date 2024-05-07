a4aaaa1e09b6d390cc899a4289196513df843ca8
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-06 22:48:46 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var userRouter = require('./routes/user');
 var task_listRouter = require('./routes/task_list');
 var taskRouter = require('./routes/task');
-var cors = require('cors');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/user', userRouter);
 app.use('/task_list', task_listRouter);
 app.use('/task', taskRouter);
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
