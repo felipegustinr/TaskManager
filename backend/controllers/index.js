6ea45611ab0d46ec0eb0e89676c61d469bcdec67
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-07 16:31:51 -0500

    Update index.js

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,10 +1,12 @@
 const userController = require('./userController');
 const task_listController = require('./task_listController');
 const taskController = require('./taskController');
-const rolController = require('./rolController')
+const rolController = require('./rolController');
+const loginController = require('./loginController');
 module.exports = {
     userController,
     task_listController,
     taskController,
-    rolController
+    rolController,
+    loginController
 };
