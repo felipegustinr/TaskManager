d6f3033c3572e27d318524bb0e751344860e933a
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-07 15:23:12 -0500

    Update rol.sql

--- database/rol.sql
+++ database/rol.sql
@@ -1,16 +1,9 @@
 -- Table: public.rol
-
 -- DROP TABLE IF EXISTS public.rol;
-
-CREATE TABLE IF NOT EXISTS public.rol
-(
-    id bigint NOT NULL DEFAULT nextval('rol_id_seq'::regclass),
+CREATE TABLE IF NOT EXISTS public.rol (
+    id bigserial NOT NULL,
     name text COLLATE pg_catalog."default" NOT NULL,
     description text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT rol_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.rol
-    OWNER to admin;
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.rol OWNER to admin;
