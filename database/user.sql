ebe0ab9a882075bb8e9cd13b514958cc02620f6b
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-07 15:23:40 -0500

    Update user.sql

--- database/user.sql
+++ database/user.sql
@@ -1,23 +1,12 @@
 -- Table: public.user
-
 -- DROP TABLE IF EXISTS public."user";
-
-CREATE TABLE IF NOT EXISTS public."user"
-(
-    id bigint NOT NULL DEFAULT nextval('user_id_seq'::regclass),
+CREATE TABLE IF NOT EXISTS public."user" (
+    id bigserial NOT NULL,
     name text COLLATE pg_catalog."default" NOT NULL,
     email text COLLATE pg_catalog."default" NOT NULL,
     password text COLLATE pg_catalog."default" NOT NULL,
     rol_id bigint NOT NULL,
     CONSTRAINT user_pkey PRIMARY KEY (id),
-    CONSTRAINT user_rol_id_fkey FOREIGN KEY (rol_id)
-        REFERENCES public.rol (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."user"
-    OWNER to admin;
+    CONSTRAINT user_rol_id_fkey FOREIGN KEY (rol_id) REFERENCES public.rol (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+) TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public."user" OWNER to admin;
