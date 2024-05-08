9aa4ea8cf3251259b519c324c3e89f2130e3ae11
Felipe-Rivas <dfgustin.4560@unicesmag.edu.co>
2024-05-08 02:05:58 -0500

    Create login.service.ts

---
+++ frontend/src/app/services/login/login.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>('http://localhost:3000/user/login', { email, password });
+  }
+  isLoggedIn(): boolean {
+    
+    const token = localStorage.getItem('token');
+    return !!token; 
+  }
+}
+
